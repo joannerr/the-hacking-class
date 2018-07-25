1696c12fdfcccc5f4026c2a7bc18044f602954a7
joannerr <39775118+joannerr@users.noreply.github.com>
2018-07-25 23:30:36 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
 
 # - ALIMENTER BDD STUDENTS -
-# 10.times do
-# 	student = Student.create(first_name: Faker::OnePiece.island, last_name: Faker::Dog.name)
-# end
+10.times do
+	student = Student.create(first_name: Faker::OnePiece.island, last_name: Faker::Dog.name)
+end
 
 # - ALIMENTER BDD COURSES -
-# 3.times do
-	# course = Course.create(subject: Faker::GreekPhilosophers.name)
-# end
+3.times do
+	course = Course.create(subject: Faker::GreekPhilosophers.name)
+end
 
 # - REMPLIR COLONNE COURSE_ID -
 10.times do |i|
